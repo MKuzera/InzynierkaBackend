0cf6c87af979e4e9022ae7852feed2204965c4da
MK410668 <mateusz.qzera@gmail.com>
2024-11-16 11:04:04 +0100

    fix

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const AuthService = require('./services/AuthService');
-const UserService = require('./services/UserService');
-
-const app = express();
-
-app.use(bodyParser.json());
-
-app.post('/api/login', (req, res) => {
-    AuthService.login(req, res);
-});
-
-app.get('/api/about', (req, res) => {
-    AuthService.about(req, res);
-});
-
-app.get('/getallusers', (req, res) => {
-    UserService.getAllUsers(req, res);
-});
+// const AuthService = require('./services/AuthService');
+// const UserService = require('./services/UserService');
+//
+// const app = express();
+//
+// app.use(bodyParser.json());
+//
+// app.post('/api/login', (req, res) => {
+//     AuthService.login(req, res);
+// });
+//
+// app.get('/api/about', (req, res) => {
+//     AuthService.about(req, res);
+// });
+//
+// app.get('/getallusers', (req, res) => {
+//     UserService.getAllUsers(req, res);
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
