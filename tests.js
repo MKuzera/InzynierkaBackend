2509fdffb9364dd3e7b5f83ccdd1fcbde8d5811c
MK410668 <mateusz.qzera@gmail.com>
2025-01-17 18:04:54 +0100

    fix

--- tests.js
+++ tests.js
@@ -1,5 +1,9 @@
 const UserController = require('./Tests/UserControllerQueryTests');
 
-const result = UserController.addUserTest();
-console.log(result);
-
+UserController.addUserTest()
+    .then(result => {
+        console.log(result); // Zwróci true lub false, w zależności od wyniku
+    })
+    .catch(err => {
+        console.log(err); // Obsłuży błąd, jeśli wystąpi
+    });
