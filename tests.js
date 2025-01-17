1e4e60dab170f7b6615ed921d63c16725bfd07c6
MK410668 <mateusz.qzera@gmail.com>
2025-01-17 19:30:32 +0100

    debug

--- tests.js
+++ tests.js
@@ -336,7 +336,6 @@ class Tests {
         const organizerID = 1;
 
         return new Promise((resolve) => {
-            console.log("weszlo");
             ConferenceService.addConferenceQuery(title, description, location, organizers, tags, price, date, link, organizerID, (err, result) => {
                 if (err) {
                     resolve(false);
@@ -344,7 +343,6 @@ class Tests {
                 }
 
                 const conferenceId = result.insertId;
-                console.log(conferenceId);
                 ConferenceService.getConferenceQuery(conferenceId, (err, conference) => {
                     if (err || !conference || conference.title !== title || conference.description !== description || conference.location !== location || conference.tags !== tags) {
                         resolve(false);
@@ -468,6 +466,7 @@ class Tests {
         return new Promise((resolve) => {
             ConferenceService.addConferenceQuery(title, description, location, organizers, tags, price, date, link, organizerID, (err, result) => {
                 if (err) {
+                    console.log(err);
                     resolve(false);
                     return;
                 }
@@ -477,12 +476,14 @@ class Tests {
                 ConferenceService.getConferenceQuery(conferenceId, (err, conference) => {
                     if (err || !conference) {
                         resolve(false);
+                        console.log(err);
                         return;
                     }
 
                     ConferenceService.deleteConferenceQuery(conferenceId, (err) => {
                         if (err) {
                             resolve(false);
+                            console.log(err);
                         } else {
                             resolve(true);
                         }
