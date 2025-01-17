73e2ddbffacfc45daa5124e34feb5af05e5c8cc3
MK410668 <mateusz.qzera@gmail.com>
2025-01-17 18:06:17 +0100

    fix

--- Tests/UserControllerQueryTests.js
+++ Tests/UserControllerQueryTests.js
@@ -5,15 +5,17 @@ class UserController {
         return new Promise((resolve, reject) => {
             UserService.addUserQuery("TEST", "TEST", "TEST", "user", (err, result) => {
                 if (err) {
-                    return reject(false); // Jeśli wystąpił błąd, odrzuć promise
+                    console.log(err);
+                    return reject(false);
                 }
 
                 // Teraz sprawdzamy, czy użytkownik został dodany
                 UserService.getAllUsersQuery((err, users) => {
                     if (err) {
+                        console.log(err);
                         return reject(false); // Jeśli wystąpił błąd, odrzuć promise
                     }
-
+                    console.log("checking if exists")
                     const userExists = users.some(user => user.username === "TEST" && user.email === "TEST");
                     resolve(userExists); // Jeśli użytkownik został znaleziony, rozwiąż promise z wartością true/false
                 });
